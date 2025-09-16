b357811f294584bf930a65ba2b7bafb2a8e924f9
JuanMFR <jmfrdz98@gmail.com>
2025-09-16 17:56:07 -0300

    Initial commit

---
+++ src/Funciones/ProductoTableModel.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Funciones;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author JuanmaPC
+ */
+public class ProductoTableModel extends AbstractTableModel{
+    private final String[] columnNames = {"Nombre", "Categoría", "Precio"};
+    private final List<Producto> productos;
+
+    public ProductoTableModel() {
+        this.productos = new ArrayList<>();
+    }
+
+    @Override
+    public int getRowCount() {
+        return productos.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+            case 1:
+                return String.class;
+            case 2:
+                return Double.class;
+            default:
+                return Object.class;
+        }
+    }
+}
